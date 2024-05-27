0f7082d78644b52ad7fc803d7f87255320443d61
Nadezhda Fort <nadejda.profiwoodru@yandex.ru>
2024-05-27 18:09:16 +0300

    Add homework 3, Interfaces

---
+++ hw003_Interfaces/src/task1/main/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type Formatter interface {
+	Format(s string) string
+}
+type BaseTextFormatter struct{}
+type BoldFormatter struct{}
+type ItalicFormatter struct{}
+type CodeFormatter struct{}
+type ChainFormatter struct {
+	formatters []Formatter
+}
+
+func (t BaseTextFormatter) Format(text string) string {
+	return text
+}
+func (b BoldFormatter) Format(text string) string {
+	return fmt.Sprintf("**%s**", text)
+}
+func (i ItalicFormatter) Format(text string) string {
+	return fmt.Sprintf("_%s_", text)
+}
+func (c CodeFormatter) Format(text string) string {
+	return fmt.Sprintf("`%s`", text)
+}
+func (chain *ChainFormatter) AddFormatter(formatter Formatter) {
+	chain.formatters = append(chain.formatters, formatter)
+}
+func (chain ChainFormatter) Format(text string) string {
+	for _, formatter := range chain.formatters {
+		text = formatter.Format(text)
+	}
+	return text
+}
+
+func main() {}
